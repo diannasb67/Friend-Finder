b0dc9ae50be7555b0179e1d2b5b7485a279d8fbb
dsb67 <diannasb67@gmail.com>
2019-02-10 19:02:01 -0800

    updated homework

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,13 @@
+//GET request to display JSON of all possible friends
+app.get('/api/friends', function (req, res) {
+    res.json('friends');
+})
+
+// POST request handle incoming survey results.  
+app.post('/api/friends', function (req, res) {
+    var newfriends = req.body.friends;
+    console.log(newfriends);
+    characters.push(newfriends);
+    res.json(friends);
+});
+
---
+++ server.js
@@ -0,0 +1,40 @@
+//Dependencies
+
+var express = require("express");
+var path = require("path");
+
+//Express app
+var app = express();
+var PORT = 3000;
+
+
+// Middleware to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+//person Data
+
+
+//Routes
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, "./app/public/home.html"));
+})
+
+app.get('/survey', function (req, res) {
+    res.sendFile(path.join(__dirname, "./app/public/survey.html"));
+})
+
+app.listen(3000, function () {
+    console.log("Listening");
+})
+
+
+
+
+
+
+
+
